620687888bde12bfd6862aa9db4e91e360e83f8c
David Marby <david@dmarby.se>
2019-10-26 23:01:36 +0200

    Add main package and Makefile

--- go.mod
+++ go.mod
@@ -4,7 +4,11 @@ go 1.13
 
 require (
 	github.com/gorilla/mux v1.7.3
+	github.com/infosum/statsd v2.1.2+incompatible
+	github.com/jamiealquiza/envy v1.1.0
 	github.com/mediocregopher/radix/v3 v3.4.0
+	github.com/spf13/cobra v0.0.5 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
 	golang.org/x/xerrors v0.0.0-20191011141410-1b5146add898 // indirect
 	nhooyr.io/websocket v1.7.2
 )
---
+++ main.go
@@ -0,0 +1,174 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+	"time"
+
+	"github.com/infosum/statsd"
+	"github.com/mullvad/message-queue/pubsub"
+	"github.com/mullvad/message-queue/queue"
+
+	"github.com/jamiealquiza/envy"
+	"github.com/mullvad/message-queue/api"
+)
+
+var (
+	metrics *statsd.Client
+	p       *pubsub.PubSub
+	q       *queue.Queue
+)
+
+func main() {
+	// Set up commandline flags
+	listen := flag.String("listen", ":8080", "listen address")
+	bufferSize := flag.Int("buffer-size", 100, "client buffer size")
+	redisAddress := flag.String("redis-address", "redis://127.0.0.1:6379", "redis address, may contain authentication details")
+	channels := flag.String("channels", "", "comma-delimited list of channels to listen and broadcast to")
+	statsdAddress := flag.String("statsd-address", "127.0.0.1:8125", "statsd address to send metrics to")
+
+	// Parse environment variables
+	envy.Parse("MQ")
+
+	// Parse commandline flags
+	flag.Parse()
+
+	if *channels == "" {
+		log.Fatalf("no channels configured")
+	}
+
+	channelList := strings.Split(*channels, ",")
+
+	log.Printf("starting message-queue")
+
+	// Initialize metrics
+	var err error
+	metrics, err = statsd.New(statsd.TagsFormat(statsd.Datadog), statsd.Prefix("mq"), statsd.Address(*statsdAddress))
+	if err != nil {
+		log.Fatalf("Error initializing metrics %s", err)
+	}
+	defer metrics.Close()
+
+	// Set up context for shutting down
+	shutdownCtx, shutdown := context.WithCancel(context.Background())
+	defer shutdown()
+
+	// Set up the pubsub listener
+	p, err = pubsub.New(*redisAddress)
+	if err != nil {
+		log.Fatal("error initializing pubsub", err)
+	}
+
+	// Set up the queue
+	q = queue.New(shutdownCtx, *bufferSize)
+
+	// Set up the message passing from redis pubsub to the queue
+	err = setupChannels(shutdownCtx, channelList)
+	if err != nil {
+		log.Fatal("error initializing queue", err)
+	}
+
+	// Create a ticker for metrics
+	ticker := time.NewTicker(time.Minute)
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				collectMetrics()
+			case <-shutdownCtx.Done():
+				ticker.Stop()
+				return
+			}
+		}
+	}()
+
+	// Start and listen on http
+	api := &api.API{
+		Queue: q,
+	}
+
+	server := &http.Server{
+		Addr:    *listen,
+		Handler: api.Router(),
+	}
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil {
+			log.Println("shutting down the http server", err)
+			shutdown()
+		}
+	}()
+
+	log.Printf("http server listening on %s", *listen)
+
+	// Wait for shutdown or error
+	err = waitForInterrupt(shutdownCtx)
+	log.Println("shutting down", err)
+
+	// Shut down http server
+	serverCtx, serverCancel := context.WithTimeout(context.Background(), time.Second*30)
+	defer serverCancel()
+	if err := server.Shutdown(serverCtx); err != nil {
+		log.Println("error shutting down", err)
+	}
+}
+
+func setupChannels(ctx context.Context, channels []string) error {
+	for _, channel := range channels {
+		in, err := p.Subscribe(channel)
+		if err != nil {
+			return err
+		}
+
+		out, err := q.CreateChannel(channel)
+		if err != nil {
+			return err
+		}
+
+		go channelWorker(ctx, in, out)
+	}
+
+	return nil
+}
+
+func channelWorker(ctx context.Context, in <-chan []byte, out chan<- []byte) {
+	defer func() {
+		close(out)
+	}()
+
+	for {
+		select {
+		case msg, open := <-in:
+			if !open {
+				return
+			}
+
+			out <- msg
+		case <-ctx.Done():
+			return
+		}
+	}
+}
+
+func collectMetrics() {
+	metrics.Gauge("subscribers", q.SubscriberCount())
+}
+
+func waitForInterrupt(ctx context.Context) error {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+	select {
+	case sig := <-c:
+		return fmt.Errorf("received signal %s", sig)
+	case <-ctx.Done():
+		return errors.New("canceled")
+	}
+}
